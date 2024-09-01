99c2234bdfdb50202f98e5a556e236aa91359b03
daniel felipe <daniellfb16@gmail.com>
2024-09-01 16:10:55 -0500

    hi

---
+++ main.js
@@ -0,0 +1,11 @@
+const hola = 0/0;
+console.log(hola);
+
+const string = "hola mundo";
+console.log(string);
+
+const suma = 5+5;
+console.log(tipos_de_datos);
+
+const resta = 5-5;
+console.log(tipos_de_datos);
